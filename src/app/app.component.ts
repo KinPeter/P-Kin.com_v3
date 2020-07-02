50b2867e72f374cbb0cfc9180892f74e6573768f
Peter Kin <kinpeter85@gmail.com>
2020-07-02 08:41:15 +0200

    Set some basic config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'pk-root',
   template: `
     <!--The content below is only a placeholder and can be replaced.-->
     <div style="text-align:center" class="content">
